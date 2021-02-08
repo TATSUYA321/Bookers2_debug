74b1b166438e7bf19d24e1e2ea753cea1f4bf0e8
TATSUYA321 <naaag0210@yahoo.co.jp>
2021-02-08 09:33:05 +0000

    Addいいねとコメント

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books, dependent: :destroy
 
-  attachment :profile_image, destroy: false
 
-  validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
+  validates :name, uniqueness: true, length: { in: 2..20}
   validates :introduction, length: {maximum: 50}
+
+  has_many :books, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  attachment :profile_image
+
+  def favorited?(book)
+    self.favorites.exists?(book_id: book.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
-  root 'homes#top'
-  get 'home/about' => 'homes#about'
+  resources :users, only:[:show,:index,:edit,:update]
+  root to:  "homes#top"
+ get "/homes/about" => "homes/about"
+  resources :books, only:[:index,:show,:create,:edit,:update,:destroy] do
+    resource :favorites, only:[:create,:destroy]
+  resources :book_comments, only:[:create, :destroy]
+ end
 end
